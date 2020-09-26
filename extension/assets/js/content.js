193bb2f68494fef406f0d31d8e45df4040d28dcf
renisalcedo <salcedoreynaldo5@gmail.com>
2020-09-26 15:35:37 -0400

    Added method for grabbing all content in tab with covid in the content

--- extension/assets/js/content.js
+++ extension/assets/js/content.js
@@ -1,6 +1,33 @@
-const paragraphs = document.getElementsByTagName('p')
+const setCovidContent = () => {
+    const foundOccurences = []
+    const allTags = ['p', 'h1', 'h2', 'h3', 'h4', 'h5','h6', 'small']
 
-for (let i = 0; i < paragraphs.length; i++) {
-    let currParagraph = paragraphs[i]
-    console.log(currParagraph.innerHTML)
+    /*
+        Iterates over every tag to make a call to getCovidContent and append data
+    */
+    for (let i = 0; i < allTags.length; i++) {
+        let currTag = allTags[i]
+        getCovidContent(currTag)
+    }
+
+    /*
+        Would iterate over the content ad detect the covid keyword and append occurences
+        @param tag - String tag from where in the page to get the content
+    */
+    function getCovidContent(tag) {
+        const tagContent = document.getElementsByTagName(tag)
+
+        for (let i = 0; i < tagContent.length; i++) {
+            let currContent = tagContent[i]
+            let content = currContent.innerHTML
+            let normalizedContent = content.toLowerCase()
+
+            // Tag property would get added to our list if contains covid in the content
+            if (normalizedContent.indexOf("covid") !== -1) {
+                foundOccurences.push(currContent)
+            }
+        }
+    }
+
+    return foundOccurences;
 }

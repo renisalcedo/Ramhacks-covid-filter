dee88e7d9d8c174b6c3a8395272a6cb662f702ec
renisalcedo <salcedoreynaldo5@gmail.com>
2020-09-27 08:49:02 -0400

    Added firebase to firebase controller

--- extension/assets/js/firebaseController.js
+++ extension/assets/js/firebaseController.js
@@ -1,9 +1,7 @@
 var db = firebase.database();
 
-var rcfRef = db.ref('ramhacks-covid-filter');
-rcfRef.on('value', function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-      var childData = childSnapshot.val();
-      console.log(childData);
-    });
+var rcfRef = db.ref();
+
+rcfRef.once("value", function(data) {
+    console.log(data.val())
 });
--- extension/assets/js/htmlContent.js
+++ extension/assets/js/htmlContent.js
@@ -12,9 +12,14 @@ function HTMLEvents() {
     function addCss(elem) {
         const infoContainer = createInfoContainer()
         $(`${elem.tagName}:contains(${elem.innerText})`).hover(
-            () => $(infoContainer).toggle()
+            () => {
+                $(infoContainer).show()
+                $(infoContainer).mouseleave(
+                    () => $(infoContainer).hide()
+                )
+            }
         )
-    
+
         elem.style['border-left'] = `2px solid ${colors.danger}`
         elem.style['border-right'] = `2px solid ${colors.danger}`
         elem.style['border-radius'] = "5%"
@@ -28,10 +33,14 @@ function HTMLEvents() {
 
     function createInfoContainer() {
         const infoContainer = document.createElement("div");
+        infoContainer.style['width'] = '10rem'
+        infoContainer.style['height'] = '10rem'
+        infoContainer.style['text-align'] = 'center'
         infoContainer.style['display'] = 'none'
-        infoContainer.style['background-color'] = '#eee'
+        infoContainer.style['background-color'] = '#fff'
+        infoContainer.style['box-shadow'] = "2px 2px #888888"
         infoContainer.style['position'] = 'absolute'
-        infoContainer.innerHTML = '<p>Some information here</p>';
+        infoContainer.innerHTML = '<p>Some information here</p> <a href="http://www.google.com">More Info</a>';
 
         return infoContainer
     }
